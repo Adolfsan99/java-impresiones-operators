a9420ba0a92563d78a12dd5993e236c349889bfa
Adolfo Sánchez Lopéz <81250601+Adolfsan99@users.noreply.github.com>
2023-04-27 17:11:47 -0500

    Add files via upload

---
+++ src/elpeperules.java
@@ -0,0 +1,14 @@
+public class elpeperules {
+        public static void main(String[] args) {
+
+            int number1 = 2;
+            int number2 = 4;
+
+            System.out.println("Hello world!");
+            System.out.println("Multiplicacion = " + number1*number2);
+            System.out.println("Division = " + number2/number1);
+            System.out.println("Modulo = " + number1%number2);
+            System.out.println("Suma = " + (number1+number2));
+
+        }
+}
